2745a66151d97c3c25e825ecb999f5da39b7b1ac
Nina Vaneeva <ninelamericanel@gmail.com>
2025-10-26 18:38:02 +0400

    designed slider section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Hero from '../components/Hero/Hero';
-import BannerSlider from '../components/BannerSlider';
+import BannerSlider from '../components/BannerSlider/BannerSlider';
 import Locations from '../components/Locations';
 import Features from '../components/Features';
 
