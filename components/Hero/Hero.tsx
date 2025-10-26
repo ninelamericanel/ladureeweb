b96e82752ffd23693d5a5068e772d70e05d96e2f
Nina Vaneeva <ninelamericanel@gmail.com>
2025-10-26 17:37:04 +0400

    align content

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -21,12 +21,16 @@ export default function Hero() {
         alt="macarons background"
         width={100}
         height={100}
-      	sizes="100vw"/>
+        sizes="100vw" />
       <div className={styles.content}>
+      <div className={styles.textContent}>
         <motion.h1 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }}>{data.h1}</motion.h1>
-        <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.3 }}>{data.desc}</motion.p>
+        <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.3 }} className='extraText'>{data.desc}</motion.p>
       </div>
-      <Button theme='light' type='link' href={data.link} desc={data.buttonDesc}></Button>
+      <div className={styles.button}>
+      <Button theme='light' type='link' href={data.link} desc={data.buttonDesc}/>
+      </div>
+      </div>
     </section>
   );
 }
